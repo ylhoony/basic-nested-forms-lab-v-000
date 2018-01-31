799cfe31ff94a80e10ecab65063c9063c58e569f
Hoon Lee <yl.hoony@gmail.com>
2018-01-31 19:44:57 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,12 @@ class RecipesController < ApplicationController
 
   private
   def recipe_params
-    params.require(:recipe).permit(:title)
+    params.require(:recipe).permit(
+      :title,
+      ingredient_attributes: [
+        :name,
+        :quantity
+      ]
+    )
   end
 end
